780467fbeb079338b28916297fdd99486360cc1f
SkyNet-PKN <SkyNet0PKN@gmail,com>
2024-07-09 16:44:52 +0800

    update v0.4.1

--- PPTMate.cpp
+++ PPTMate.cpp
@@ -1,6 +1,5 @@
 ﻿#include <windows.h>
 #include <iostream>
-#include <string>
 #include <atlstr.h>
 
 using namespace std;
@@ -8,28 +7,52 @@ using namespace std;
 CString PowerPnt_Path,
 		ArchiveFolder_Path;
 
-int IsCreateSubfolder;
+int IsCreateSubfolder,
+	IsPowerpntPathOverride;
 
+static int ReadReg(HKEY rootKey, TCHAR* regPath, TCHAR* regKey, TCHAR* regValue)
+{
+	HKEY hKey;
+	int ret = RegOpenKeyEx(rootKey, regPath, 0, KEY_EXECUTE, &hKey);
+	if (ret != ERROR_SUCCESS)
+	{
+		cout << "[Error] RegOpenKeyEx failed" << endl;
+		return 1;
+	}
 
-BOOL IsPathExist(const CString& csPath)
+	DWORD dwType = REG_SZ;
+	DWORD cbData = 256;
+	ret = RegQueryValueEx(hKey, regKey, NULL, &dwType, (LPBYTE)regValue, &cbData);
+	if (ret == ERROR_SUCCESS)
+	{
+		RegCloseKey(hKey);
+		return 0;
+	}
+	else
+	{
+		cout << "[Error] RegQueryValueEx failed" << endl;
+		RegCloseKey(hKey);
+		return 1;
+	}
+}
+
+static inline BOOL IsPathExist(const CString& csPath)
 {
 	int nRet = _taccess(csPath, 0);
 	return 0 == nRet || EACCES == nRet;
 }
 
-inline CString GetFileNameFromPath(string path) {
-	int index = path.find_last_of('\\');
-	string res = path.substr(index + 1, path.size() - (index + 1));
-	return (CString)res.c_str();
+static inline CString GetFileNameFromPath(CString path) {
+	int index = path.ReverseFind('\\');
+	return path.Right(path.GetLength() - (index + 1));
 }
 
-inline CString GetFileFolderFromPath(string path) {
-	int index = path.find_last_of('\\');
-	string res = path.substr(0, index);
-	return (CString)res.c_str();
+static inline CString GetFileFolderFromPath(CString path) {
+	int index = path.ReverseFind('\\');
+	return path.Left(index);
 }
 
-inline CString GetTimeStr() {
+static inline CString GetTimeStr() {
 	SYSTEMTIME currentTime;
 	char res[20];
 
@@ -46,18 +69,30 @@ int main(int argc, char* argv[]) {
 	CString cwdPath;
 	GetModuleFileName(NULL, cwdPath.GetBuffer(MAX_PATH), MAX_PATH);
 	//wprintf(L"path:%s\n", szPath.GetString());
-	SetCurrentDirectory(GetFileFolderFromPath((string)(CT2A(cwdPath))));
+	SetCurrentDirectory(GetFileFolderFromPath(cwdPath));
 
-	CString tempIsCreateSubfolder;
+	CString tempIsCreateSubfolder, tempIsPowerpntPathOverride;
 	GetPrivateProfileString(_T("Path"), _T("powerpoint"), _T("C:\\Program Files\\Microsoft Office\\root\\Office16\\POWERPNT.EXE"), PowerPnt_Path.GetBuffer(MAX_PATH), MAX_PATH, _T(".\\config.ini"));
-	GetPrivateProfileString(_T("Path"), _T("archive_folder"), _T("D:\\PPTBACKUP"), ArchiveFolder_Path.GetBuffer(MAX_PATH), MAX_PATH, _T(".\\config.ini"));
+	GetPrivateProfileString(_T("Path"), _T("powerpoint_path_override"), _T("false"), tempIsPowerpntPathOverride.GetBuffer(MAX_PATH), MAX_PATH, _T(".\\config.ini"));
+	GetPrivateProfileString(_T("Path"), _T("archive_folder"), _T("F:\\PPTBACKUP"), ArchiveFolder_Path.GetBuffer(MAX_PATH), MAX_PATH, _T(".\\config.ini"));
 	GetPrivateProfileString(_T("Setting"), _T("is_create_subfolder"), _T("true"), tempIsCreateSubfolder.GetBuffer(MAX_PATH), MAX_PATH, _T(".\\config.ini"));
+	if (tempIsPowerpntPathOverride == "true") IsPowerpntPathOverride = 1;
+	else IsPowerpntPathOverride = 0;
 	if (tempIsCreateSubfolder == "true") IsCreateSubfolder = 1;
 	else IsCreateSubfolder = 0;
-	wprintf(L"%s %s %s\n", PowerPnt_Path.GetString(), ArchiveFolder_Path.GetString(), tempIsCreateSubfolder.GetString());
 
-	if (!IsPathExist(PowerPnt_Path)) {
-		printf("[Error] Powerpoint executable file not vaild\n");
+	if(IsPowerpntPathOverride == 0)
+	{
+		TCHAR val[256];
+    	ReadReg((HKEY)HKEY_LOCAL_MACHINE, CA2T("SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\App Paths\\powerpnt.exe"), CA2T(""), val);
+		PowerPnt_Path = val;
+		wprintf(L"%s\n", PowerPnt_Path.GetString());
+	}
+
+	if (!IsPathExist(PowerPnt_Path))
+	{
+		wprintf(L"[Error] Powerpoint executable file (%s) not vaild\n", PowerPnt_Path.GetString());
+		MessageBox(NULL, L"Powerpoint可执行文件路径无效。\n请先打开Powerpoint应用程序并在程序中打开文件。", L"PPTMate无法启动Powerpoint", MB_OK | MB_ICONWARNING);
 		return 0;
 	}
 
@@ -66,7 +101,7 @@ int main(int argc, char* argv[]) {
 	CString filePath, fileName, subfolderName, destFolderPath, destFilePath;
 
 	filePath = argv[1];
-	fileName = GetFileNameFromPath((string)(CT2A(filePath)));
+	fileName = GetFileNameFromPath(filePath);
 	subfolderName = GetTimeStr();
 	if (IsCreateSubfolder) destFolderPath.Format(L"%s\\%s", ArchiveFolder_Path.GetString(), subfolderName.GetString());
 	else destFolderPath = ArchiveFolder_Path;
@@ -81,23 +116,27 @@ int main(int argc, char* argv[]) {
 		destFilePath.GetString()
 	);
 
-	CString curCmd;
-
 	CreateDirectory(ArchiveFolder_Path.GetString(), NULL);
 	if (IsCreateSubfolder) CreateDirectory(destFolderPath.GetString(), NULL);
-	CopyFile(filePath.GetString(), destFilePath.GetString(), FALSE);
-	//curCmd.Format(L"cmd.exe /c mkdir \"%s\"", destFolderPath.GetString());
-	//WinExec(CT2A(curCmd), SW_HIDE);
-	//wprintf(L"%s\n", curCmd.GetString());
 
 	CopyFile(filePath.GetString(), destFilePath.GetString(), FALSE);
-	//curCmd.Format(L"cmd.exe /c copy %s %s", filePath.GetString(), destFolderPath.GetString());
-	//WinExec(CT2A(curCmd), SW_HIDE);
-	//wprintf(L"%s\n", curCmd.GetString());
 
-	curCmd.Format(L"cmd.exe /c cd /d \"%s\" && start %s \"%s\"", GetFileFolderFromPath((string)(CT2A(PowerPnt_Path))).GetString(), GetFileNameFromPath((string)(CT2A(PowerPnt_Path))).GetString(), filePath.GetString());
-	WinExec(CT2A(curCmd), SW_HIDE);
-	//wprintf(L"%s\n", curCmd.GetString());
-
+	CString curCmd;
+	curCmd.Format(L"/c cd /d \"%s\" && start %s \"%s\"", GetFileFolderFromPath(PowerPnt_Path).GetString(), GetFileNameFromPath(PowerPnt_Path).GetString(), filePath.GetString());
+	wprintf(L"%s\n", curCmd.GetString());
+	auto ret = ShellExecute(
+		NULL,
+		CA2T("open"),
+		CA2T("cmd.exe"),
+		curCmd.GetString(),
+		NULL,
+		SW_HIDE
+	);
+	if ((INT_PTR)ret <= 32)
+	{
+		printf("[Error] Failed to open Powerpoint");
+		MessageBox(NULL, L"Powerpoint可执行文件路径无效。\n请先打开Powerpoint应用程序并在程序中打开文件。", L"PPTMate无法启动Powerpoint", MB_OK | MB_ICONWARNING);
+		return 0;
+	}
 	return 0;
 }
--- README.md
+++ README.md
@@ -7,16 +7,17 @@ A simple tool which can save the copies of  Powerpoint files to a certain folder
 
 ## Todo
 
-- Automatically recognize the location of Powerpoint executable file.
 - Add more command line options in future.
 
 ## Release Note
 
-**0.4.0** Depart configurations into `.ini` files & Add a switch in configuration to control whether create a subfolder named by the date & Big format standardization.
+**0.4.1** Now the program will automatically recognize the location of Powerpoint executable file. You can choose to override by editing `config.ini`.
+
+**0.4.0** Depart configurations into `config.ini`. Add a switch in configuration to control whether create a subfolder named by the date & Big format standardization.
 
 **0.3.1** Increase opening speed and fix some bugs.
 
-**0.3.0** Fix the problem of console windows flashing.
+**0.3.0** Fix the problem of command-line interface flashing.
 
 **0.2.1** Save the file in the subfolders with the date and change the icon.
 
